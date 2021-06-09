6f41ded91950c2a37c1c330a6e350ea8a4195acd
yalitzab <yalitzabueno@gmail.com>
2021-06-08 21:09:23 -0700

    test contact component

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -7,21 +7,28 @@ import "@testing-library/jest-dom/extend-expect";
 import Nav from "..";
 
 const categories = [
-  { name: 'portraits', description: 'Portraits of people in my life' }
-]
+  { name: "portraits", description: "Portraits of people in my life" },
+];
+// Declare props
 const mockCurrentCategory = jest.fn();
 const mockSetCurrentCategory = jest.fn();
+const mockContactSelected = jest.fn();
+const mockSetContactSelected = jest.fn();
 
 afterEach(cleanup);
 
 describe("Nav component", () => {
   // baseline test
   it("renders", () => {
-    render(<Nav 
-      categories={categories}
-      setCurrentCategory={mockSetCurrentCategory}
-      currentCategory={mockCurrentCategory}
-    />);
+    render(
+      <Nav
+        categories={categories}
+        setCurrentCategory={mockSetCurrentCategory}
+        currentCategory={mockCurrentCategory}
+        contactSelected={mockContactSelected}
+        setContactSelected={mockSetContactSelected}
+      />
+    );
   });
 
   // snapshot test
